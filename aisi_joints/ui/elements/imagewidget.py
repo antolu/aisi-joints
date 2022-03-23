ed6d2c9a389ec82a8e8d86b6caff458b88c64ff8
Anton Lu <anton96lu@gmail.com>
2022-03-23 11:46:09 +0100

    draw bounding boxes

--- aisi_joints/ui/elements/imagewidget.py
+++ aisi_joints/ui/elements/imagewidget.py
@@ -1,10 +1,11 @@
-from typing import Optional, Tuple
+from typing import Optional
 
-from PyQt5.QtWidgets import QWidget
-from pyqtgraph import GraphicsLayoutWidget, PlotItem, ImageItem, GraphicsView, ViewBox
+import cv2 as cv
 import numpy as np
 import pandas as pd
-import cv2 as cv
+from PyQt5.QtGui import QPen, QColorConstants as QColor
+from PyQt5.QtWidgets import QWidget
+from pyqtgraph import ImageItem, GraphicsView, ViewBox, PlotDataItem
 
 from ..utils import run_in_main_thread
 
@@ -18,17 +19,34 @@ class ImageWidget(GraphicsView):
         self.vb.invertY()
         self.setCentralItem(self.vb)
 
-        self.item: ImageItem = None
+        self.item: ImageItem = ImageItem(axisOrder='row-major')
+        self.box = PlotDataItem(pen=QPen(QColor.Black, 4), connect='all', )
+
+        self.pen_green = QPen(QColor.Green, 4)
+        self.pen_red = QPen(QColor.Red, 4)
+
+        self.vb.addItem(self.item)
+        self.vb.addItem(self.box)
 
     @run_in_main_thread
     def show_image(self, sample: pd.DataFrame):
         image = cv.imread(sample.filepath)
 
-        if self.item is None:
-            self.item = ImageItem(image, axisOrder='row-major')
-            self.vb.addItem(self.item)
+        x0 = sample.x0
+        x1 = sample.x1
+        y0 = sample.y0
+        y1 = sample.y1
+        label = sample.cls
+
+        box = np.array([[x0, y0], [x1, y0], [x1, y1], [x0, y1], [x0, y0]])
+
+        self.item.setImage(image)
+        self.box.setData(box)
+
+        if label == 'DEFECT':
+            self.box.setPen(self.pen_red)
         else:
-            self.item.setImage(image)
+            self.box.setPen(self.pen_green)
 
     def clear_plot(self):
         if self.item is None:
