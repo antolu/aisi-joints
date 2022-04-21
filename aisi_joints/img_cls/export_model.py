bd48a3567fa35068fd82765e21f08c1a086c01b3
Anton Lu <anton96lu@gmail.com>
2022-04-21 19:44:28 +0200

    add export model for img_cls

---
+++ aisi_joints/img_cls/export_model.py
@@ -0,0 +1,38 @@
+import logging
+from argparse import ArgumentParser
+
+import tensorflow as tf
+from .models import get_model
+from .config import Config
+
+log = logging.getLogger(__name__)
+
+
+def export_model(config: Config, checkpoint_dir: str, output_dir: str):
+    base_model, model = get_model(config.base_model)
+
+    log.info(f'Reading checkpoint from {checkpoint_dir}.')
+    checkpoint = tf.train.latest_checkpoint(checkpoint_dir)
+
+    model.load_weights(checkpoint)
+
+    log.info(f'Writing model to {output_dir}.')
+    model.save(output_dir)
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+
+    parser.add_argument('config', help='Path to config.yml')
+    parser.add_argument('--checkpoint-dir', dest='checkpoint_dir',
+                        default='checkpoints', type=str,
+                        help='Path to directory with trained checkpoints. '
+                             'Will use latest checkpoint from the directory '
+                             'unless a specific file is selected.')
+    parser.add_argument('-o', '--output-dir', dest='output_dir', type=str,
+                        default='exported_models',
+                        help='Directory to save exported model to.')
+
+    args = parser.parse_args()
+
+    conf = Config(args.config)
