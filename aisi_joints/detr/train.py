883f44b9a58c65716d7dc82cc1bbf16df3e68b0b
Anton Lu <anton96lu@gmail.com>
2022-05-10 22:47:16 +0200

    add detr config.yml

--- aisi_joints/detr/train.py
+++ aisi_joints/detr/train.py
@@ -2,6 +2,7 @@ from argparse import ArgumentParser
 from functools import partial
 
 import torch
+import yaml
 from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks import ModelCheckpoint
 from torch.utils.data import DataLoader
@@ -26,8 +27,9 @@ def collate_fn(feature_extractor, batch: dict):
     return batch
 
 
-def train(img_folder: str):
-    model = Detr(lr=1e-4, lr_backbone=1e-5, weight_decay=1e-4, num_classes=2)
+def train(config: dict, img_folder: str):
+    model = Detr(lr=config['lr'], lr_backbone=config['lr_backbone'],
+                 weight_decay=config['weight_decay'], num_classes=2)
     feature_extractor = model.feature_extractor
 
     train_dataset = CocoDetection(img_folder, 'train',
@@ -35,16 +37,19 @@ def train(img_folder: str):
     val_dataset = CocoDetection(img_folder, 'validation',
                                 feature_extractor=feature_extractor)
 
-    train_dataloader = DataLoader(train_dataset, collate_fn=partial(collate_fn, feature_extractor),
-                                  batch_size=4, shuffle=True, num_workers=8)
-    val_dataloader = DataLoader(val_dataset, collate_fn=partial(collate_fn, feature_extractor),
-                                batch_size=1, shuffle=False, num_workers=4)
+    train_dataloader = DataLoader(
+        train_dataset, collate_fn=partial(collate_fn, feature_extractor),
+        batch_size=config['batch_size'], shuffle=True,
+        num_workers=config['workers'])
+    val_dataloader = DataLoader(
+        val_dataset, collate_fn=partial(collate_fn, feature_extractor),
+        batch_size=1, shuffle=False, num_workers=4)
 
     checkpoint_callback = ModelCheckpoint('checkpoints',
                                           'model-{epoch:02d}-{val_loss:.2f}',
                                           monitor='validation_loss')
 
-    trainer = Trainer(gpus=1, max_steps=10000, gradient_clip_val=1.0,
+    trainer = Trainer(max_epochs=config['epochs'], gradient_clip_val=1.0,
                       callbacks=[checkpoint_callback])
     trainer.fit(model, train_dataloader, val_dataloader)
 
@@ -52,8 +57,14 @@ def train(img_folder: str):
 if __name__ == '__main__':
     parser = ArgumentParser()
 
-    parser.add_argument('data_dir', help='Path to COCO dataset folder.')
+    parser.add_argument('-d', '--data', dest='data_dir',
+                        help='Path to COCO dataset folder.')
+    parser.add_argument('config',
+                        help='Path to config.yml')
 
     args = parser.parse_args()
 
-    train(args.data_dir)
+    with open(args.config) as f:
+        conf = yaml.full_load(f)
+
+    train(conf, args.data_dir)
