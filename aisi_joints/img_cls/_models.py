ebda840fd0cfddcbae7a4649b6b0e44d9f22f18d
Anton Lu <anton96lu@gmail.com>
2022-05-17 17:14:06 +0200

    add more models for image classification

--- aisi_joints/img_cls/_models.py
+++ aisi_joints/img_cls/_models.py
@@ -20,22 +20,22 @@ def get_model(model_name: str, fc_hidden_dim: int = 2048,
     elif model_name == 'vgg19':
         base_model: Model = tf.keras.applications.VGG19(
             include_top=False, weights='imagenet',
-            input_tensor=Input(299, 299, 32))
+            input_tensor=Input(shape=(299, 299, 32)))
         preprocess_fn = tf.keras.applications.vgg19.preprocess_input
     elif model_name == 'resnet101v2':
         base_model: Model = tf.keras.applications.ResNet101V2(
             include_top=False, weights='imagenet',
-            input_tensor=Input(299, 299, 32))
+            input_tensor=Input(shape=(299, 299, 32)))
         preprocess_fn = tf.keras.applications.resnet_v2.preprocess_input
     elif model_name == 'resnet152v2':
         base_model: Model = tf.keras.applications.ResNet152V2(
             include_top=False, weights='imagenet',
-            input_tensor=Input(299, 299, 32))
+            input_tensor=Input(shape=(299, 299, 32)))
         preprocess_fn = tf.keras.applications.resnet_v2.preprocess_input
     elif model_name == 'efficientnetv2l':
         base_model: Model = tf.keras.applications.EfficientNetV2L(
             include_top=False, weights='imagenet',
-            input_tensor=Input(299, 299, 3))
+            input_tensor=Input(shape=(299, 299, 3)))
         preprocess_fn = tf.keras.applications.efficientnet_v2.preprocess_input
     else:
         raise NotImplementedError
