cd7648159ca1ecaebc5ebc3c01ec925cac5a3e71
Anton Lu <anton96lu@gmail.com>
2022-05-23 19:17:51 +0200

    add install_requires to self_supervised

---
+++ aisi_joints/self_supervised/__init__.py
@@ -0,0 +1,11 @@
+"""
+This module contains code related to the self supervised approach for the
+joints model.
+"""
+install_requires = [
+    'git+https://github.com/antolu/self_supervised.git',
+    'pillow',
+    'torch',
+    'torchvision',
+    'pytorch_lightning'
+]
