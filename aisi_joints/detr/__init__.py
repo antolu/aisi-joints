fb34baf6af671696154bdcd47534640b09bb9027
Anton Lu <anton96lu@gmail.com>
2022-08-11 17:21:01 +0200

    update dependencies

--- aisi_joints/detr/__init__.py
+++ aisi_joints/detr/__init__.py
@@ -1,7 +1,4 @@
-from os import path
-from .._utils.extras_require import check_dependencies
-
-HERE = path.split(__file__)[0]
-
-check_dependencies(path.join(HERE, 'requirements.txt'))
-
+"""
+This module implements the DE:TR object detection architecture for
+joint classification.
+"""
